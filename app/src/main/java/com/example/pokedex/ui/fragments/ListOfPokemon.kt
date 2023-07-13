7857751fb93fbe0fe1065c04baaace7adb4b0081
EdgarSalgado <edgar.salgado.osorio@gmail.com>
2023-07-13 00:40:24 -0600

    implementation of Location

--- app/src/main/java/com/example/pokedex/ui/fragments/ListOfPokemon.kt
+++ app/src/main/java/com/example/pokedex/ui/fragments/ListOfPokemon.kt
@@ -1,11 +1,22 @@
 package com.example.pokedex.ui.fragments
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.os.Looper.getMainLooper
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
@@ -17,6 +28,7 @@ import com.example.pokedex.ui.adapter.PokemonAdapter
 import com.example.pokedex.ui.models.toDetailPokemonModel
 import com.example.pokedex.ui.viewmodels.ListOfPokemonViewModel
 import com.example.pokedex.ui.viewmodels.PokemonActions
+import com.google.android.gms.location.*
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -28,6 +40,10 @@ class ListOfPokemon : Fragment() {
     private val viewModel: ListOfPokemonViewModel by viewModels()
     private lateinit var adapter: PokemonAdapter
     private var listAux = mutableListOf<Pokemon>()
+
+    private val requestingLocationUpdates = true
+    private var count: Int = 0
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,11 +58,19 @@ class ListOfPokemon : Fragment() {
         binding.btShowPokemon.setOnClickListener {
             viewModel.getNewPokemon()
         }
+            requestPermission()
+
     }
 
     override fun onResume() {
         super.onResume()
         viewModel.getListOfPokemon()
+
+        if (requestingLocationUpdates) {
+            count = 0
+            startLocationUpdates()
+        }
+
     }
 
     private fun bindActionLiveData() {
@@ -96,4 +120,90 @@ class ListOfPokemon : Fragment() {
         loadingBar(false)
         navigateToDetailPokemon(pokemon)
     }
+
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            count += 1
+            if(count > 1) {
+                viewModel.getNewPokemon()
+                vibratePhone()
+            }
+        }
+    }
+
+    private var mLocationRequest: LocationRequest =
+        LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 1000)
+            .setWaitForAccurateLocation(false)
+            .setMinUpdateIntervalMillis(1500)
+            .setMaxUpdateDelayMillis(2500)
+            .setMinUpdateDistanceMeters(10.0f)
+            .build()
+
+    private var fusedLocationClient: FusedLocationProviderClient? = null
+
+    private val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        when {
+            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                getLastLocation()
+            }
+        }
+    }
+
+    private fun requestPermission() {
+        if(ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            getLastLocation()
+        } else {
+            locationPermissionRequest.launch(arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION))
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLastLocation() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+       // fusedLocationClient?.lastLocation?.addOnSuccessListener { location : Location? ->
+               // location?.let {
+                   // onSetMap(it.latitude, it.longitude)
+                //}
+            //}
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startLocationUpdates() {
+        fusedLocationClient?.requestLocationUpdates(mLocationRequest, locationCallback, getMainLooper())
+    }
+
+    private fun vibratePhone() {
+        val vibrator = requireActivity().getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+            // create vibrator effect with the constant EFFECT_HEAVY_CLICK
+            val vibrationEffect = VibrationEffect.createOneShot(2000, VibrationEffect.EFFECT_HEAVY_CLICK)
+
+            // it is safe to cancel other vibrations currently taking place
+            vibrator.cancel();
+
+            vibrator.vibrate(vibrationEffect);
+        } else {
+            vibrator.vibrate(2000)
+        }
+    }
+    override fun onPause() {
+        super.onPause()
+        stopLocationUpdates()
+    }
+
+    private fun stopLocationUpdates() {
+        fusedLocationClient?.removeLocationUpdates(locationCallback)
+    }
+
+
+
+
+
+
+
+
+
 }
